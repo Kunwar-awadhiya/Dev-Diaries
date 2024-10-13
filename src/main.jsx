afb1fa5ff9eaf3b00a477352eb8b522634ac0acf
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2024-10-13 21:37:05 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { RouterProvider} from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>,
+)
